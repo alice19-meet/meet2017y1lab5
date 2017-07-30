9c67becf573d08540a8717e7d32901e49fc24d4e
alice19-meet <alice19@meet.mit.edu>
2017-07-30 13:52:27 +0300

    Stopping here

--- fun2.py
+++ fun2.py
@@ -1,9 +1,37 @@
-print("*")
-print("*")
-print("*")
-print("*")
-print("*")
 
-print("*****")
+####c1='~'
+####c2='!'
+####    
+def draw_1d(n, char):
+    print(char*n)
+####draw_1d(12,c1)
+####draw_1d(6,c2)
+####draw_1d(24,'+')
+##
+##
+####n=5
+####m=12
+####char=('*')
+##def draw_2d(n,m,char1):
+##    for num in range(n):
+##        draw_1d(m, char1)
+##draw_2d(3,23,'*')
+##draw_2d(7,3,'x')
 
-draw_1d(3)
+#part1 bonus
+##def add_numbers(start, end, skip):
+##    c=0
+##    for number in range(start,end+1,skip):
+##        print(number)
+##        c=c+number
+##    return c
+##add_numbers(0,100,2)
+
+#part2 bonus
+n=(5)
+m=(12)
+border=('@')
+fill=('x')
+def special_draw_2d(n,m,border,fill):
+    for numbers in range(n):
+        print(numbers)
